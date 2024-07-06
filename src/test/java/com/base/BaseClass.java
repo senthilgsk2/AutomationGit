7774f58ca9affe65fa858730e41983dc1223b610
Senthil Kumar G <mgsenthilmgs@gmail.com>
2024-07-06 07:53:15 +0530

    created base class

---
+++ src/test/java/com/base/BaseClass.java
@@ -0,0 +1,16 @@
+package com.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		System.out.println("Done...");
+	}
+}
