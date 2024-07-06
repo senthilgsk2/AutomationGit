caccb16db4556a066ec9f7b55a9543bb2c220580
Senthil Kumar G <mgsenthilmgs@gmail.com>
2024-07-06 09:24:37 +0530

    created base class

--- src/test/java/com/base/BaseClass.java
+++ src/test/java/com/base/BaseClass.java
@@ -14,6 +14,8 @@ public class BaseClass {
 		System.out.println("created dev branch");
 
 		System.out.println("code updated by git a to dev");
+		
+		System.out.println("merge updated from git a");
 
 	}
 }
